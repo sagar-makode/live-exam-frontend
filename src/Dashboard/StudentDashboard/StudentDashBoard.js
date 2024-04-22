7eae340902c05ba2af906640a76b9f305eee5c48
sagar-makode <makodepatil1@gmail.com>
2024-04-22 23:49:26 +0530

    added loading

--- src/Dashboard/StudentDashboard/StudentDashBoard.js
+++ src/Dashboard/StudentDashboard/StudentDashBoard.js
@@ -1,255 +1,73 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchStudentTestresult, fetchTests } from '../../actions/testActions';
-import avtar from "../../assets/pngegg.png"
-import { Spinner } from 'react-bootstrap';
+import { fetchAllCretater, subscribeToTeacher, unsubscribetoTeacher} from '../../actions/subscribers';
+import img from "../../assets/pngegg.png"
+import { Button, Spinner } from 'react-bootstrap';
+
+function ALLCreater() {
 
 
-import { Link } from 'react-router-dom';
-import TestSeries from './TestSeries';
-import TestResults from './TestResults';
-import AllTests from './AllTests';
-import Subcriptions from './Subcriptions';
-import ALLCreater from './ALLCreater';
+  const allCreterwithSubStatus = useSelector(state => state.subcriptiondata.allCreterwithSubStatus);
+  const subcribeSuccess = useSelector(state => state.subcriptiondata.subcribeSuccess);
+  const unsubcribeSuccess = useSelector(state => state.subcriptiondata.unsubcribeSuccess);
+  console.log(unsubcribeSuccess);
+  const [loading, setLoading] = useState(false); // Add loading state
 
-function StudentDashBoard() {
+console.log(allCreterwithSubStatus);
+    const dispatch = useDispatch()
+    useEffect(() => {
+      dispatch(fetchAllCretater())
+      setLoading(false)
+  
+    }, [dispatch,subcribeSuccess,unsubcribeSuccess]);
+  
 
+    const handleSubscribe = async (id) => {
+      setLoading(true)
+      // Dispatch action to subscribe student to teacher
+  dispatch(subscribeToTeacher(id))
+    }
 
-
-  const dispatch = useDispatch()
-
-  const studentProfileData = useSelector(state => state.dashboard.userData);
-
-
-
-  const [activeTab, setActiveTab] = useState('dashboard');
-  const [loading, setLoading] = useState(false);
-
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
-
-
-
-  useEffect(() => {
-
+  const handleUnsubscribe = (id) => {
     setLoading(true)
-    dispatch(fetchTests());
-    dispatch(fetchStudentTestresult())
-   
-    setLoading(false)
-  }, [dispatch]);
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+      dispatch(unsubscribetoTeacher(id));
   };
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
+  useEffect(() => {
+    if (subcribeSuccess || unsubcribeSuccess) {
+      setLoading(false);
+    }
+  }, [subcribeSuccess, unsubcribeSuccess]);
 
 
   return (
-
     <div>
-      {loading ? (
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      ) : (<div className={`main-container ${isDarkMode ? 'dasboardbodydark' : 'dasboardbody'}`}>
-        <nav className={`sidebar ${isSidebarOpen ? 'open' : 'close'}`}>
-          <header>
-            <div className="image-text">
-              <span className="image">
-                <img src={avtar} alt="" />
-              </span>
-
-              <div className="text logo-text">
-                <span className="name">{studentProfileData.name}</span>
-                <span className="profession">{studentProfileData.role}</span>
-              </div>
-            </div>
-
-            <i className='bx bx-chevron-right toggle' onClick={toggleSidebar}></i>
-          </header>
-
-          <div className="menu-bar">
-            <div className="menu">
-
-              <li className="search-box">
-                <i className='bx bx-search icon'></i>
-                <input type="text" placeholder="Search..." />
-              </li>
-
-              <ul className="menu-links">
-                <li className={`nav-link ${activeTab === 'dashboard' ? 'active' : ''}`} onClick={() => handleTabClick('dashboard')}>
-                  <Link >
-                    <i className='bx bx-home-alt icon'></i>
-                    <span className="text nav-text">Dashboard</span>
-                  </Link>
-
-
-                </li>
-
-                <li className={`nav-link ${activeTab === 'liveTest' ? 'active' : ''}`} onClick={() => handleTabClick('liveTest')}>
-                  <Link  >
-                    <i className='bx bx-notepad icon'></i>
-
-                    <span className="text nav-text">Live Test</span>
-
-                  </Link>
-
-
-                </li>
-
-
-
-                <li className={`nav-link ${activeTab === 'testSeries' ? 'active' : ''}`} onClick={() => handleTabClick('testSeries')}>
-                  <Link >
-                    <i className='bx bxs-folder-open icon'></i>
-
-                    <span className="text nav-text">Test Series</span>
-                  </Link>
-                </li>
-
-                <li className={`nav-link ${activeTab === 'testresult' ? 'active' : ''}`} onClick={() => handleTabClick('testresult')}>
-                  <Link >
-                    <i className='bx bx-bar-chart icon'></i>
-                    <span className="text nav-text">Test Result</span>
-                  </Link>
-                </li>
-
-                <li className={`nav-link ${activeTab === 'subscriptions' ? 'active' : ''}`} onClick={() => handleTabClick('subscriptions')}>
-                  <Link >
-                   
-                      <span className="material-symbols-outlined icon">
-                        subscriptions
-                      </span>
-                   
-                    <span className="text nav-text">Subscriptions</span>
-                  </Link>
-                </li>
-
-
-
-                <li className={`nav-link ${activeTab === 'allcreater' ? 'active' : ''}`} onClick={() => handleTabClick('allcreater')}>
-                  <Link >
-                   
-                      <span className="material-symbols-outlined icon">
-                        groups
-                      </span>
-                   
-                    <span className="text nav-text">ALL Creaters</span>
-                  </Link>
-                </li>
-                <li className={`nav-link ${activeTab === 'wishlist' ? 'active' : ''}`} onClick={() => handleTabClick('wishlist')}>
-                  <Link >
-                    <i className='bx bx-bookmark-heart icon'></i>
-                    <span className="text nav-text">Wishlist</span>
-
-                  </Link>
-                </li>
-
-
-                <li className="nav-link">
-                  <Link >
-                    <i className='bx bx-bell icon'></i>
-                    <span className="text nav-text">Notifications</span>
-                  </Link>
-                </li>
-
-              </ul>
-            </div>
-
-            <div className="bottom-content">
-              <li className="">
-                <Link >
-                  <i className='bx bx-log-out icon'></i>
-                  <span className="text nav-text">Logout</span>
-                </Link>
-              </li>
-
-              <li className="mode">
-                <div className="sun-moon">
-                  <i className='bx bx-moon icon moon'></i>
-                  <i className='bx bx-sun icon sun'></i>
-                </div>
-                <span className="mode-text text">Dark mode</span>
-
-                <div className="toggle-switch" onClick={toggleDarkMode}>
-                  <span className="switch" ></span>
-                </div>
-              </li>
-
+      <h1> ALL Creator</h1>
+      <div>
+        {allCreterwithSubStatus.map((subscription, index) => (
+          <div key={index} style={{ display: 'inline-block', margin: '0 10px' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <img src={img} alt="Creator" style={{ borderRadius: '50%', width: '100px', height: '100px' }} />
+              <div>{subscription.name}</div>
+              <div>Subscribers: {subscription.subscribers.length}</div>
+              {subscription.isSubscribed ? (
+                 <Button variant="danger" onClick={() => handleUnsubscribe(subscription._id)} disabled={loading}>
+                 {loading ? <Spinner animation="border" size="sm" /> : 'Unsubscribe'} 
+               </Button>
+                // <button onClick={()=>handleUnsubscribe(subscription._id)}>Unsubscribe</button>
+              ) : (
+                <Button variant="primary" onClick={() => handleSubscribe(subscription._id)} disabled={loading}>
+                {loading ? <Spinner animation="border" size="sm" /> : 'Subscribe'} 
+              </Button>
+                // <button onClick={()=>handleSubscribe(subscription._id)}>Subscribe</button>
+                )}
             </div>
           </div>
-
-        </nav>
-
-
-        <div className="main-content" style={{ marginLeft: isSidebarOpen ? '250px' : '90px', transition: 'margin-left 0.3s ease' }}>
-          {/* Render content based on activeTab */}
-          {activeTab === 'dashboard' && (
-            <div>
-              {/* Dashboard content */}
-              <h1>Dashboard Content</h1>
-            </div>
-          )}
-          {activeTab === 'liveTest' && (
-            <div>
-              <AllTests />
-            </div>
-
-          )}
-          {activeTab === 'testSeries' && (
-            <div>
-              {/* Test Series content */}
-              <TestSeries />
-            </div>
-          )}
-
-          {activeTab === 'testresult' && (
-            <div>
-
-              <TestResults />
-            </div>
-          )}
-          {activeTab === 'subscriptions' && (
-            <div>
-
-              <Subcriptions />
-            </div>
-          )}
-          {activeTab === 'allcreater' && (
-            <div>
-
-              <ALLCreater />
-            </div>
-          )}
-
-          {activeTab === 'wishlist' && (
-            <div>
-              {/* Test Series content */}
-              <h1>Wishlist Content</h1>
-            </div>
-          )}
-        </div>
-      </div>)}
-
-
-
+        ))}
+      </div>
 
     </div>
-
-
-
-
-
   )
 }
 
-export default StudentDashBoard
+export default ALLCreater
